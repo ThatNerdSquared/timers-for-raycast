472c0220aafdf1368b8bf42e10f01ae8ba811cd6
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2022-02-22 21:33:02 -0800

    wip: add basic structure for persistent timers

--- src/timerUtils.ts
+++ src/timerUtils.ts
@@ -1,7 +1,15 @@
+import { environment } from "@raycast/api";
 import { exec } from "child_process";
+import { writeFileSync } from "fs";
 
 export default async function startTimer(timeInSeconds: number) {
-  const command = `sleep ${timeInSeconds} && afplay /System/Library/Sounds/Submarine.aiff && osascript -e 'display notification "'"Timer complete"'" with title "Ding!"'`;
+  const notification = "Untitled";
+  const fileName = environment.supportPath + "/" + new Date().toISOString() + "---" + timeInSeconds + ".txt";
+  console.log(fileName);
+  const masterName = fileName.replaceAll(":", "__");
+  console.log(masterName);
+  writeFileSync(masterName, notification);
+  const command = `sleep ${timeInSeconds} && if [ -f "${masterName}" ]; then afplay /System/Library/Sounds/Submarine.aiff && osascript -e 'display notification "'"Timer complete"'" with title "Ding!"' && rm "${masterName}"; else echo "Timer deleted"; fi`;
   exec(command, (error, stderr) => {
     if (error) {
       console.log(`error: ${error.message}`);
