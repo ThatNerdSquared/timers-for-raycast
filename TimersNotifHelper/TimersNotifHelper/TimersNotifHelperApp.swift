284ae4f857bbd4602cddc06e05921db3a7625fad
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2024-05-19 18:01:48 -0700

    feat: integrate TimersNotifHelper into extension

--- TimersNotifHelper/TimersNotifHelper/TimersNotifHelperApp.swift
+++ TimersNotifHelper/TimersNotifHelper/TimersNotifHelperApp.swift
@@ -1,17 +1,8 @@
-//
-//  TimerNotifHelperApp.swift
-//  TimerNotifHelper
-//
-//  Created by Nathan Yeung on 2024-05-16.
-//
-
 import UserNotifications
 
 func scheduleNotif(timerName: String) async -> Void {
     // obtain notification permissions
     let notifCenter = UNUserNotificationCenter.current()
-    let notifDelegate = TimerNotifDelegate();
-    notifCenter.delegate = notifDelegate;
     do {
         try await notifCenter.requestAuthorization(options: [.alert])
     } catch {
@@ -19,18 +10,6 @@ func scheduleNotif(timerName: String) async -> Void {
         exit(1);
     }
     
-    // set up actions
-    let dismissTimerAction = UNNotificationAction(
-        identifier: "dismissTimerAction",
-        title: "Dismiss Timer"
-    )
-    let category = UNNotificationCategory(
-        identifier: "timerNotif",
-        actions: [dismissTimerAction],
-        intentIdentifiers: []
-    )
-    notifCenter.setNotificationCategories([category])
-
     let content = UNMutableNotificationContent()
     content.title = "Ding!"
     content.subtitle = "Timer \"\(timerName)\" complete."
@@ -58,35 +37,5 @@ struct TimerNotifHelper {
     static func main() async {
         let timerName = CommandLine.arguments[1];
         await scheduleNotif(timerName: timerName)
-    }
-}
-
-
-class TimerNotifDelegate: NSObject, UNUserNotificationCenterDelegate {
-    func userNotificationCenter(
-        _ center: UNUserNotificationCenter,
-        didReceive response: UNNotificationResponse,
-        withCompletionHandler completionHandler: @escaping () -> Void
-    ) {
-        print("SDJFLSDJFJDDSHJHFH")
-        // pull out the buried userInfo dictionary
-        let userInfo = response.notification.request.content.userInfo
-
-        if let customData = userInfo["customData"] as? String {
-            print("Custom data received: \(customData)")
-
-            switch response.actionIdentifier {
-
-            case "dismissTimerAction":
-                // the user tapped our "show more info…" button
-                print("Show more information…")
-
-            default:
-                break
-            }
-        }
-
-        // you must call the completion handler when you're done
-        completionHandler()
     }
 }
