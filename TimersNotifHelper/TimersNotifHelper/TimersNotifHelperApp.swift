12a9e425cf67b23991cb907c6ce0d9d4b6878b61
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2024-05-23 22:44:23 -0700

    feat(notifhelper): add banner actions for dismiss, opening Manage Timers

--- TimersNotifHelper/TimersNotifHelper/TimersNotifHelperApp.swift
+++ TimersNotifHelper/TimersNotifHelper/TimersNotifHelperApp.swift
@@ -1,4 +1,31 @@
+import SwiftUI
 import UserNotifications
+
+@main
+struct TimerNotifHelper: App {
+    @State var notifDelegate: UNUserNotificationCenterDelegate = TimerNotifDelegate();
+
+    init() {
+        let notifCenter = UNUserNotificationCenter.current();
+        notifCenter.delegate = notifDelegate;
+
+        let timerName = CommandLine.arguments[1];
+        Task() {
+            await scheduleNotif(timerName: timerName)
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 60 * 3) {
+            exit(0);
+        }
+    }
+
+    var body: some Scene {
+        MenuBarExtra("TimerNotifHelper", systemImage: "app.badge") {
+            Button("Quit") {
+                exit(0);
+            }
+        }
+    }
+}
 
 func scheduleNotif(timerName: String) async -> Void {
     // obtain notification permissions
@@ -9,7 +36,24 @@ func scheduleNotif(timerName: String) async -> Void {
         print("Could not obtain notification permission!")
         exit(1);
     }
-    
+
+    // set up actions
+    let dismissTimerAction = UNNotificationAction(
+        identifier: "dismissTimerAction",
+        title: "Dismiss Timer"
+    )
+    let manageTimersAction = UNNotificationAction(
+        identifier: "manageTimersAction",
+        title: "Manage Timers..."
+    )
+    let category = UNNotificationCategory(
+        identifier: "timerNotif",
+        actions: [dismissTimerAction, manageTimersAction],
+        intentIdentifiers: []
+    )
+    notifCenter.setNotificationCategories([category])
+
+    // create banner notification
     let content = UNMutableNotificationContent()
     content.title = "Ding!"
     content.subtitle = "Timer \"\(timerName)\" complete."
@@ -19,6 +63,7 @@ func scheduleNotif(timerName: String) async -> Void {
         repeats: false
     )
 
+    // send notification
     let request = UNNotificationRequest(
         identifier: UUID().uuidString,
         content: content,
@@ -32,10 +77,26 @@ func scheduleNotif(timerName: String) async -> Void {
     }
 }
 
-@main
-struct TimerNotifHelper {
-    static func main() async {
-        let timerName = CommandLine.arguments[1];
-        await scheduleNotif(timerName: timerName)
+
+class TimerNotifDelegate: NSObject, UNUserNotificationCenterDelegate {
+    @Environment(\.openURL) var openURL
+
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void
+    ) {
+        print("Received dismiss action.")
+        switch response.actionIdentifier {
+            case "dismissTimerAction":
+                print("Dismiss action launching...")
+                openURL(URL(string: "raycast://extensions/ThatNerd/timers/dismissTimerAlert")!)
+            case "manageTimersAction":
+                print("Manage Timers action launching...")
+                openURL(URL(string: "raycast://extensions/ThatNerd/timers/manageTimers")!)
+            default:
+                break
+        }
+        completionHandler()
     }
 }
